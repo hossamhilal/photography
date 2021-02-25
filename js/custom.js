7c287ea085e851aa5a436bcbf90ee28a872f69d2
Hossam Hilal <46751584+hossamhilal@users.noreply.github.com>
2021-02-25 13:51:57 +0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -65,70 +65,9 @@
     });
 
     $('.scroll-top').click(function() {
-        $("html, body").animate({ scrollTop: 0 }, 4000);
+        $("html, body").animate({ scrollTop: 0 }, 3000);
         return false;
     });
-
-    // $(window).on("load", function(){
-    //     let slider = document.querySelector('.slider') ,
-    //         { width } = slider.getBoundingClientRect() ,
-    //         halfWidth = width / 2;
-
-    //     slider.addEventListener("mousemove", function (e) {
-    //         const xPos = e.pageX - slider.offsetLeft;
-
-    //         this.classList.remove("cursor-prev", "cursor-next");
-    //         if (xPos > halfWidth) {
-    //             this.classList.add("cursor-next");
-    //         } else {
-    //             this.classList.add("cursor-prev");
-    //         }
-    //     });
-    // });
-
-
-    // Testimonials OWL 
-    // $('.owlTestimonials').owlCarousel({
-    //     margin: 20,
-    //     autoplay: true,
-    //     loop: false,
-    //     nav: true,
-    //     dots: false,
-    //     center : false ,
-    //     autoplaySpeed : 5000,
-    //     autoplayTimeout : 5000,
-    //     smartSpeed: 5000 ,
-    //     navText: ["<i class='icofont-thin-right'></i>", "<i class='icofont-thin-left'></i>"],
-    //     responsive: {
-    //         0: {
-    //             items: 1
-    //         },
-    //         600: {
-    //             items: 2
-    //         },
-    //         1000: {
-    //             items: 2
-    //         }
-    //     }
-    // });
-
-    // // Upload File 
-    // $('.uploadFile').on('change', function(e) {
-    //     let fileName = e.target.value.split( '\\' ).pop();
-    //     console.log(fileName);
-    //     let files = $(this).parent('.uploadBox').prev('.uploadedFiles');
-    //     files.append(
-    //         '<div class="file">' +
-    //             '<h3 class="fileName">' + fileName  + '</h3>' +
-    //             '<span class="deleteFile"> <i class="icofont-ui-delete"></i> </span>' +
-    //         '</div>'
-    //     );               
-    // });
-
-    // // Delete File
-    // $(document).on('click','.deleteFile' , function(){
-    //     $(this).parent('.file').remove();
-    // });
 
     // iniat WOW Js
     new WOW().init();
